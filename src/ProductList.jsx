f9cb4682e151d49f29f1050cb005d5d6ea8e1ddd
egramza <edgramza@gmail.com>
2025-06-19 18:26:08 -0400

    Update code

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -212,6 +212,8 @@ function ProductList({ onHomeClick }) {
             ]
         }
     ];
+
+    const [addedToCart, setAddedToCart] = useState({});
     const styleObj = {
         backgroundColor: '#4CAF50',
         color: '#fff!important',
@@ -274,8 +276,39 @@ function ProductList({ onHomeClick }) {
             </div>
             {!showCart ? (
                 <div className="product-grid">
-
-
+                    {plantsArray.map((category, index) => (
+                        <div key={index}>
+                            <h1>
+                                <div>{category.category}</div>
+                            </h1>
+                            <div className="product-list">
+                                {category.plants.map((plant, plantIndex) => (
+                                <div className="product-card" key={plantIndex}>
+                                    <img 
+                                        className="product-image"
+                                        src={plant.image} 
+                                        alt={plant.name} 
+                                    />
+                                    <div className="product-title">
+                                        {plant.name}
+                                    </div>
+                                    <div className="product-description">
+                                        {plant.description}
+                                    </div>
+                                    <div className="product-cost">
+                                        {plant.cost}
+                                    </div>
+                                    <button
+                                        className="product-button"
+                                        onClick={() => handleAddToCart(plant)}
+                                    >
+                                        Add to Cart
+                                    </button>
+                                </div>
+                                ))}
+                            </div>
+                        </div>
+                    ))}
                 </div>
             ) : (
                 <CartItem onContinueShopping={handleContinueShopping} />
